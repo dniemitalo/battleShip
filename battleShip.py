7ad056befead6d0ca288f52b8ea8aa9fa5bb247f
Matt Behnke <16218746+MatthewBehnke@users.noreply.github.com>
2018-02-20 13:26:40 -0600

    Update battleShip.py

--- battleShip.py
+++ battleShip.py
@@ -10,11 +10,14 @@ class Matrix():
             '0','0','0','0','0',
             '0','0','0','0','0',
             '0','0','0','0','0']
+  shipOne = []
+  shipTwo = []
+  shipThree = []
 
 playerOne = Matrix()
 playerTwo = Matrix()
 
-def lookUpTwo(iVal, bVal):
+def lookUp(iVal, bVal):
   if iVal == 0 and bVal == 0:
     return 0 
   else:
@@ -22,11 +25,36 @@ def lookUpTwo(iVal, bVal):
 
 def printMatrix(player):
   for i in range(11):
-    print [player.grid[lookUpTwo(i,b)] for b in range(5)]
+    print [player.grid[lookUp(i,b)] for b in range(5)]
 
-def updateMatrix(player, xVal, yVal):
-  lookUpTwo  
-  
+def updateMatrix(value, xVal, yVal, player):
+  place = lookUp(yVal,xVal)  
+  player.grid[place] = value
+
+# def placeShip(player, x1,x2, y1, y2):
+#   isHorizontal = False
+#   isVertical = False
+#   if x1 == x2:
+#     isHorizontal = True
+#     isVertical = False
+#   elif y1 == y2:
+#     isVertical = True
+#     isHorizontal = False
+#   else:
+#     print 'Ship cannot be placed diagonally'
+#   if isHorizontal == True:
+#     distance = x2 - x1
+#   elif isVertical == True:
+#     distance = y2 - y1 
+#   for i in range(distance):
+#     if i == 1:
+#       updateMatrix('1',x1,y1, player)
+#     elif isHorizontal:
+#       updateMatrix('1',(x1+1),y1, player)
+#     elif isVertical:
+#       updateMatrix('1',x1,(y1+1), player)
     
+print 'playerOne'
 printMatrix(playerOne)
+print 'playerTwo'
 printMatrix(playerTwo)
