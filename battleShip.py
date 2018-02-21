aa095b1206602db360eac3c6128b85a0508a9956
jatupper <36518282+jatupper@users.noreply.github.com>
2018-02-21 13:44:03 -0600

    Update battleShip.py

--- battleShip.py
+++ battleShip.py
@@ -1,16 +1,17 @@
 class Matrix():
-  grid =   ['00','1','2','3','4','5',
-            '01','0','0','0','0','0',
-            '02','0','0','0','0','0',
-            '03','0','0','0','0','0',
-            '04','0','0','0','0','0',
-            '05','0','0','0','0','0',
-            '06','N','N','N','N','N',
-            '07','0','0','0','0','0',
-            '08','0','0','0','0','0',
-            '09','0','0','0','0','0',
-            '10','0','0','0','0','0',
-            '11','0','0','0','0','0']
+  def __init__(self):
+    self.grid =   ['00','1','2','3','4','5',
+              '01','0','0','0','0','0',
+              '02','0','0','0','0','0',
+              '03','0','0','0','0','0',
+              '04','0','0','0','0','0',
+              '05','0','0','0','0','0',
+              '06','N','N','N','N','N',
+              '07','0','0','0','0','0',
+              '08','0','0','0','0','0',
+              '09','0','0','0','0','0',
+              '10','0','0','0','0','0',
+              '11','0','0','0','0','0']
 
 playerOne = Matrix()
 playerTwo = Matrix()
@@ -37,18 +38,18 @@ def placeShip(player, x1, y1, x2, y2):
   elif x1 == x2:
     isHorizontal = False
     isVertical = True
-    distance = abs(y2 - y1 +1)
+    distance = y2 - y1 +1
   elif y1 == y2:
     isVertical = False
     isHorizontal = True
-    distance = abs(x2 - x1 + 1)
+    distance = x2 - x1 + 1
   else:
     print 'Ship cannot be placed diagonally'
-  for i in range(distance):
+  for i in range(abs(distance)):
     if isHorizontal:
-      updateMatrix('Y',(x1+i),y1, player)
+      updateMatrix('1',(x1+i),y1, player)
     elif isVertical:
-      updateMatrix('Z',x1,(y1+i), player)
+      updateMatrix('1',x1,(y1+i), player)
 
 def playerName(player):
   if player == playerOne: 
@@ -58,6 +59,26 @@ def playerName(player):
   player.name = raw_input('Who will be ' + x+'?')
   return player.name
 
+def playerShip(player):
+  print player.name + ' you are up'
+  print 'place your ship by providing coordinates'
+  for i  in range(3):
+    if i == 0:
+      print 'Place ship #1 (length of 3)'
+    elif i == 1:
+      print 'Place ship #2 (length of 2)'
+    elif i == 2:
+      print 'Place ship #3 (length of 2)'
+    x1 = int(raw_input('x1 '))
+    y1 = int(raw_input('y1 '))
+    x2 = int(raw_input('x2 '))
+    y2 = int(raw_input('y2 '))
+    placeShip(player, abs(x1), abs(y1), abs(x2), abs(y2)) 
+    if player == playerOne:
+      printMatrix(playerOne)
+    elif player == playerTwo:
+      printMatrix(playerTwo)
+
 def main():
   print 'Welcome to battleship'
   print 'The aim of the game is to sink your opponent\'s ships.'
@@ -65,7 +86,7 @@ def main():
   playerName(playerOne)
   playerName(playerTwo)
   print 'The players are', playerOne.name, 'and' ,playerTwo.name
-  print 'Player one is up. Please place your ships'
-  # placeShip(playerOne,,,,)
+  playerShip(playerOne)
+  playerShip(playerTwo)
   
-# main()
+main()
